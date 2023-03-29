34f16b7a530b8a879ed9ed2f0927f1d8f88273da
Aswin-web <ashvinachu097@gmail.com>
2023-03-29 11:40:06 +0530

    Commit

--- src/app.js
+++ src/app.js
@@ -26,17 +26,25 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [`${process.env.CLIENT_URL}`,"http://localhost:3000"],
-    credentials:true,
+    origin: [
+      `${process.env.CLIENT_URL}`,
+      "http://localhost:3000",
+      "https://careersheets.netlify.app/",
+    ],
+    credentials: true,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
-    allowedHeaders:['Access-Control-Allow-Origin','Content-Type','Authorization']
+    allowedHeaders: [
+      "Access-Control-Allow-Origin",
+      "Content-Type",
+      "Authorization",
+    ],
   })
 );
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", "https://careersheets.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
