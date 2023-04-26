7d289bcf82ccee83f4b16d3338339fa097d8f08c
Amal-web <amalsivadas08@gmail.com>
2023-04-26 21:43:09 +0530

    updated beta

--- src/controllers/college.controller.js
+++ src/controllers/college.controller.js
@@ -1,7 +1,7 @@
 const User = require("../models/user.models");
 const Education = require("../models/education.model");
 const Application = require("../models/application.models");
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
 const getUsers = async (req, res) => {
   const user = req.user._id.toString();
@@ -19,41 +19,45 @@ const getUsers = async (req, res) => {
     collegeAdmin = await User.findById(user);
 
     college = collegeAdmin.collegeName;
-    console.log(collegeAdmin);
+    
+    if(!college){
+      return res.status(501).json({message:"notselected"})
+    }
 
-   
-
-    userApplication = await Education.aggregate([
-      { $match: { collegeName: college ,collegeVerification:true} },
-      {
-        $lookup: {
-          from: "users",
-          localField: "user",
-          foreignField: "_id",
-          as: "userDetails",
+    if (collegeAdmin.collegeVerification) {
+      userApplication = await Education.aggregate([
+        { $match: { collegeName: college /*collegeVerification:true*/ } },
+        {
+          $lookup: {
+            from: "users",
+            localField: "user",
+            foreignField: "_id",
+            as: "userDetails",
+          },
         },
-      },
-      {
-        $lookup: {
-          from: "applications",
-          localField: "user",
-          foreignField: "author",
-          as: "application",
+        {
+          $lookup: {
+            from: "applications",
+            localField: "user",
+            foreignField: "author",
+            as: "application",
+          },
         },
-      },
-    ]);
+      ]);
+      return res
+      .status(200)
+      .json({ userApplication: userApplication, collegeAdmin: collegeAdmin });
+    }else{
+      return res.status(401).json({message:"College Admin is not verified"})
+    }
   } catch (error) {
     console.log(error);
   }
+
  
-
-
-  return res
-    .status(200)
-    .json({ userApplication: userApplication, collegeAdmin: collegeAdmin });
 };
 
-////adding college admin's college anf contact/////
+////adding college admin's college and contact/////
 const addCollege = async (req, res) => {
   const user = req.user._id.toString();
   const { college, contact } = req.body;
@@ -66,7 +70,7 @@ const addCollege = async (req, res) => {
   try {
     const abc = await User.findByIdAndUpdate(
       { _id: user },
-      { $set: { collegeName: college, contact: contact } }
+      { $set: { collegeName: college, contact: contact,collegeVerification:false } }
     );
     collegeAdmin = await User.findById(user);
   } catch (error) {
@@ -77,14 +81,14 @@ const addCollege = async (req, res) => {
 
 const getUserById = async (req, res) => {
   const id = req.params.id;
-  console.log(id, "idddd");
+
   let applicationDetails;
   let user;
   try {
-    applicationDetails = await Application.find({ author: id }).populate("author");
-    console.log(applicationDetails)
-
-    
+    applicationDetails = await Application.find({ author: id }).populate(
+      "author"
+    );
+   
 
     if (!applicationDetails) {
       return res.status(200).json({ message: "No appliation Details found" });
@@ -92,9 +96,7 @@ const getUserById = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-  res
-    .status(200)
-    .json({ applicationDetails: applicationDetails });
+  res.status(200).json({ applicationDetails: applicationDetails });
 };
 
 module.exports = {
